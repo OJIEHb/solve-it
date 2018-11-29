e55ca2a067b07aff771a42a8c8339ba896fcca49
Andrii Heiko <andriihe@softwareplanetgroup.com>
2018-11-29 21:51:29 +0200

    Add left side menu items

--- web/src/App.vue
+++ web/src/App.vue
@@ -12,6 +12,20 @@
         <md-app-toolbar>
           <span class="md-title">Solve it</span>
         </md-app-toolbar>
+        <md-list class="side-menu">
+          <md-list-item>
+            <md-icon md-src="./assets/icons/wolf.svg" />
+            <span class="md-list-item-text">Grey wolf</span>
+          </md-list-item>
+          <md-list-item>
+            <md-icon md-src="./assets/icons/bat.svg" />
+            <span class="md-list-item-text">Bat gang</span>
+          </md-list-item>
+          <md-list-item>
+            <md-icon md-src="./assets/icons/planet.svg" />
+            <span class="md-list-item-text">Gravitational search</span>
+          </md-list-item>
+        </md-list>
       </md-app-drawer>
 
       <md-app-content>
