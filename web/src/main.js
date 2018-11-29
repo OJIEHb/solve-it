73a90817d3c70b7f5ded482cbd616cd331662401
Andrii Heiko <andriihe@softwareplanetgroup.com>
2018-11-29 23:01:07 +0200

    Add particles animation on home page

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import VueMaterial from 'vue-material';
+import VueParticles from 'vue-particles'
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default-dark.css';
 
 Vue.use(VueMaterial);
+Vue.use(VueParticles);
 Vue.config.productionTip = false;
 
 new Vue({
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
-    Solve it
+    <div class="title">
+      <div class="md-display-3">Solve it</div>
+      <div class="md-display-1">Is service that solving optimization problem using bioinspired algorithms</div>
+    </div>
     <img class="bottom-divider" src="@/assets/bottom-divider.svg">
+    <vue-particles
+      class="particles"
+      color="#dedede"
+      :particleOpacity="0.8"
+      :particlesNumber="50"
+      shapeType="circle"
+      :particleSize="6"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.5"
+      :linesDistance="150"
+      :moveSpeed="4"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    >
+    </vue-particles>
   </div>
 </template>
 
@@ -17,5 +39,24 @@
     bottom: 0;
     right: 0;
     left: 0;
+    z-index: 1;
+  }
+
+  .title {
+    position: absolute;
+    width: 80%;
+    left: 0;
+    top: 20%;
+    right: 0;
+    margin: auto;
+    text-align: center;
+  }
+
+  .particles {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
   }
 </style>
