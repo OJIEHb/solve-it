4396c629a39ae0f81714f03f9df3d505231dda58
Andrii Heiko <andriihe@softwareplanetgroup.com>
2018-11-29 21:35:18 +0200

    Add material ui navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,54 @@
 <template>
-  <div>
-    <router-view/>
+  <div class="page-container">
+    <md-app md-mode="reveal">
+      <md-app-toolbar>
+        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">Solve it</span>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-app-toolbar>
+          <span class="md-title">Solve it</span>
+        </md-app-toolbar>
+      </md-app-drawer>
+
+      <md-app-content>
+        <router-view/>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+        menuVisible: false
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  .md-app {
+    min-height: 100vh;
+  }
+
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+
+  .md-app-content {
+    height: 100%;
+  }
+
+  .side-menu {
+    top: 64px;
+  }
+</style>
