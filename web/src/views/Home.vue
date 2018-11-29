af3bdf45020f53cb103b933a909123be053b8509
Andrii Heiko <andriihe@softwareplanetgroup.com>
2018-11-29 22:01:27 +0200

    Add bottom divider on home page

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     Solve it
+    <img class="bottom-divider" src="@/assets/bottom-divider.svg">
   </div>
 </template>
 
@@ -9,3 +10,12 @@
     name: 'home'
   }
 </script>
+
+<style lang="scss" scoped>
+  .bottom-divider {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
+</style>
