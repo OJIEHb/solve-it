ddce89e2bcbeaf95bf829a2254bae65763e5b1d0
Andrii Heiko <andriihe@softwareplanetgroup.com>
2018-11-30 23:06:38 +0200

    Add simple bat gang algorithm form

--- web/src/views/BatGang.vue
+++ web/src/views/BatGang.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>Bat Gang</div>
+  <bat-gang-form @submit="onSubmit"/>
 </template>
 
 <script>
+  import BatGangForm from '@/components/BatGangForm'
+
   export default {
+    components: {
+      BatGangForm
+    },
     name: 'bat-gang',
+    methods: {
+      onSubmit(problem) {
+        console.log(problem);
+      }
+    }
   }
 </script>
